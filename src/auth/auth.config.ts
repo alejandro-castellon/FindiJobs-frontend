62af37d6c33048b50b95afb8b1abe6096064891b
Alejandro Castellon <alejandrocastellonfer@gmail.com>
2024-05-11 01:25:25 -0400

    fix auth

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -8,9 +8,10 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
-      if (isOnDashboard && !isLoggedIn) {
-        return Response.redirect("/login");
-      } else if (!isOnDashboard && isLoggedIn) {
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
         return Response.redirect(new URL("/dashboard", nextUrl));
       }
       return true;
